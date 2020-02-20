d3a8a4b32ff4ad0eb817ef2171ceefd56abf84db
付金诗 <fuxiansen@hundun.cn>
2020-02-20 16:19:11 +0800

    添加数据集

--- test.py
+++ test.py
@@ -3,6 +3,41 @@ import pandas as pd
 import numpy as np
 import os
 
+#练习地址：https://www.cnblogs.com/rango-lhl/p/9729334.html
+#数据集地址：https://github.com/Rango-2017/Pandas_exercises
+
+'''
+#####1.开始了解你的数据
+###探索Chipotle快餐数据
+-- 将数据集存入一个名为chipo的数据框内
+-- 查看前10行内容
+-- 数据集中有多少个列(columns)？
+-- 打印出全部的列名称
+-- 数据集的索引是怎样的？
+-- 被下单数最多商品(item)是什么?
+-- 在item_name这一列中，一共有多少种商品被下单？
+-- 在choice_description中，下单次数最多的商品是什么？
+-- 一共有多少商品被下单？
+-- 将item_price转换为浮点数
+-- 在该数据集对应的时期内，收入(revenue)是多少？
+-- 在该数据集对应的时期内，一共有多少订单？
+-- 每一单(order)对应的平均总价是多少？
+'''
+
+# -- 将数据集存入一个名为chipo的数据框内
+chipo = pd.read_csv('')
 
 
+# -- 查看前10行内容
+# -- 数据集中有多少个列(columns)？
+# -- 打印出全部的列名称
+# -- 数据集的索引是怎样的？
+# -- 被下单数最多商品(item)是什么?
+# -- 在item_name这一列中，一共有多少种商品被下单？
+# -- 在choice_description中，下单次数最多的商品是什么？
+# -- 一共有多少商品被下单？
+# -- 将item_price转换为浮点数
+# -- 在该数据集对应的时期内，收入(revenue)是多少？
+# -- 在该数据集对应的时期内，一共有多少订单？
+# -- 每一单(order)对应的平均总价是多少？
 
