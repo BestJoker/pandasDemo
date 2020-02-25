8b1d42288ce842ee73a765c32e85cd5d926a8c4d
付金诗 <fuxiansen@hundun.cn>
2020-02-25 10:22:25 +0800

    完成第三题

--- test.py
+++ test.py
@@ -170,8 +170,20 @@ print (drinks.columns)
 print (drinks['continent'].unique())
 
 # -- 哪个大陆(continent)平均消耗的啤酒(beer)更多？
+df = drinks[['continent','beer_servings']].groupby(by=['continent']).mean().sort_values(by=['beer_servings'],ascending=False)
+print (df.head(1))
+
 # -- 打印出每个大陆(continent)的红酒消耗(wine_servings)的描述性统计值
+print (drinks.groupby(by=['continent']).wine_servings.describe())
+
+
 # -- 打印出每个大陆每种酒类别的消耗平均值
+print (drinks.groupby(by=['continent']).mean())
+
 # -- 打印出每个大陆每种酒类别的消耗中位数
+print (drinks.groupby(by=['continent']).median())
+
 # -- 打印出每个大陆对spirit饮品消耗的平均值，最大值和最小值
+print (drinks.groupby(by=['continent']).spirit_servings.describe())
+
 
