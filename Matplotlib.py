cde02cdbb2d21197a1cb577313f1b484e66007a0
付金诗 <fuxiansen@hundun.cn>
2020-02-26 08:50:45 +0800

    no message

--- Matplotlib.py
+++ Matplotlib.py
@@ -4,4 +4,18 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-PROJECT_PATH = os.path.j
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join(PROJECT_ROOT,'多人讨论汇总数据.xlsx')
+
+df = pd.read_excel(path)
+print (df)
+print (df.columns)
+
+#总围观人数，社员围观人数曲线
+plt.plot(df['日期'],df['总围观人数'])
+plt.show()
+
+
+
+
