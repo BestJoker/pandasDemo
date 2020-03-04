aa5240ac2b5b3f1b8a85fc181145aeb5b78ca412
付金诗 <fuxiansen@hundun.cn>
2020-03-04 10:04:37 +0800

    更新日期到03-03

--- initail.py
+++ initail.py
@@ -35,7 +35,8 @@ def getTopicWithDateStr(dateStr):
         '02-28': '疫情过后，哪些方向更值得投资？',
         '02-29': '零售行业如何在疫情中寻找突破？',
         '03-01': '零售行业如何把握疫情后的先机？',
-        '03-02': '直播带货成为生存之策，李佳琦的成功我能否复制？'
+        '03-02': '直播带货成为生存之策，李佳琦的成功我能否复制？',
+        '03-03': '疫情波及投资，如何理性评估当下的风险和收益？'
     }
     topic = topic_dic[dateStr]
     return topic
@@ -180,7 +181,7 @@ def initail(dateStr):
 #     dateStr = x.strftime('%m-%d')
 #     initail(dateStr)
 
-dateStr = '03-02'
+dateStr = '03-03'
 initail(dateStr)
 
 
